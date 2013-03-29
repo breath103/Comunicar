a6511ba57dae4f38245433da3a4236ade0305c72
breath103 <breath103@gmail.com>
2013-03-30 02:47:10 +0900

    add contents loader. fix main_text bug

--- public/js/FBDiary/facebook_contents_loader.js
+++ public/js/FBDiary/facebook_contents_loader.js
@@ -1,139 +1,91 @@
-function initObjectWithDict(object,dict){
-    for(var key in dict){
-        if(typeof(dict[key]) != "function")
-            object[key] = dict[key];
-    }
+/*
+function createPostDiv(v){
+    $(".site-container").append( generatePostDivHTML(v) ).children().last().click(function(){
+        $(".left-pad").html(JSON.stringify(v));
+    });
+}
+function createPostDivAndPrepend(v){
+    $(".site-container").prepend(generatePostDivHTML(v))
+        .children()
+        .last()
+        .click(function(){
+            $(".left-pad").html(JSON.stringify(v));
+        });
 }
 
-function Status(json){
-    if(json){
-        initObjectWithDict(this,json);
+
+if (localStorage.latest_post) { //if is there a cached response already exist.
+    var loadedPosts = [];
+
+    if(localStorage.loaded_posts)
+        loadedPosts = JSON.parse(localStorage.loaded_posts);
+    else{
+        loadedPosts = [];
     }
-    this.content_type = "Status";
-}
-Status.prototype = {
-    getCreatedTime : function(){
-        return new Date(this.time * 1000);
-    }
-};
 
-function Photo(json){
-    if(json){
-        initObjectWithDict(this,json);
-    }
-    this.content_type = "Photo";
-}
-Photo.prototype = {
-    getCreatedTime : function(){
-        return new Date(this.created * 1000);
-    }
-};
+    var latest_post = JSON.parse(localStorage.latest_post);
 
-function Link(json){
-    if(json){
-        initObjectWithDict(this,json);
-    }
-    this.content_type = "Link";
-}
-Link.prototype = {
-    getCreatedTime : function(){
-        return new Date(this.created_time * 1000);
-    }
-};
+    _.each(loadedPosts, createPostDiv);
 
+    console.log(latest_post);
 
+    $(".Loading").css({"display":"none"});
+    var loadingPrevFBPostLoop = function(response){
+        loadedPosts =
+            _.chain(loadedPosts)
+                .union(response.data)
+                .uniq(function(v){return v.id;})
+                .sortBy(function(v){return -(new Date(v.created_time)).getTime();})
+                .value();
 
-function FacebookContentsLoader() {
+        _(response.data.reverse()).each(createPostDivAndPrepend);
 
-}
-FacebookContentsLoader.prototype = {
-    getAllStatuses : function(callback) {
-        var resultsArray = [];
-        function getPostAfterTime(index) {
-            var fqlQuery = null;
-            fqlQuery = " SELECT message,source,status_id,time " + " FROM status " + " WHERE uid = me() " + " ORDER BY time DESC " + " limit " + index * 100 + "," + 100;
-            console.log(fqlQuery);
-
-            FB.api('fql', {
-                q : fqlQuery
-            }, function(res) {
-
-                if (!res || res.error) {
-                    callback(res.error,null);
-                    return;
-                }
-                if (res && res.data && res.data.length > 0) {
-                    resultsArray = _.union(resultsArray,res.data);
-                    getPostAfterTime(index + 1);
-                } else {
-                    resultsArray.forEach(function(v,i){
-                        resultsArray[i] = new Status(v);
-                    });
-                    callback(null,resultsArray);
-                }
-            });
+        if(response.data.length > 0 && response.paging.previous){
+            console.log(response);
+            $.getJSON(response.paging.previous,loadingPrevFBPostLoop);
         }
-
-        getPostAfterTime(0);
-    },
-    getAllPhotos : function(callback) {
-        var resultsArray = [];
-        function getPhotoWithIndex(index) {
-            var fqlQuery = null;
-            fqlQuery = " SELECT object_id,pid,src,src_big,created,target_id,target_type,comment_info,src_small,src_small_width,src_small_height,caption,link,comment_info " + " FROM photo " + " WHERE owner=me() " +
-                " limit " + index * 100 + ","+100;
-            console.log(fqlQuery);
-
-            FB.api('fql', {
-                q : fqlQuery
-            }, function(res) {
-                if (!res || res.error) {
-                    callback(res.error,null);
-                    return;
-                }
-                if (res && res.data && res.data.length > 0) {
-                    console.log("new Data : "+res.data.length);
-                    resultsArray = _.union(resultsArray,res.data);
-                    console.log("resultsArray  : "+resultsArray.length);
-                    getPhotoWithIndex(index + 1);
-                } else {
-                    resultsArray.forEach(function(v,i){
-                        resultsArray[i] = new Photo(v);
-                    });
-                    callback(null,resultsArray);
-                }
-            });
+        else {
+            console.log(loadedPosts);
+            localStorage.loaded_posts = JSON.stringify(loadedPosts);
+            localStorage.latest_post  = JSON.stringify(_.first(loadedPosts));
+            $(".Loading").fadeOut();
         }
-
-        getPhotoWithIndex(0);
-    },
-    getAllLinks : function(callback) {
-        var resultsArray = [];
-        function getLinkWithIndex(index) {
-            var fqlQuery = null;
-            fqlQuery = " SELECT link_id, owner, owner_comment, created_time, title, summary, url, image_urls FROM link WHERE owner=me() "+
-                " limit " + index * 100 + ","+100;
-            console.log(fqlQuery);
-            FB.api('fql', {
-                q : fqlQuery
-            }, function(res) {
-                if (!res || res.error) {
-                    callback(res.error,null);
-                    return;
-                }
-                if (res && res.data && res.data.length > 0) {
-                    resultsArray = _.union(resultsArray,res.data);
-                    getLinkWithIndex(index + 1);
-                } else {
-                    resultsArray.forEach(function(v,i){
-                        resultsArray[i] = new Link(v);
-                    });
-                    callback(null,resultsArray);
-                }
-            });
-        }
-
-        getLinkWithIndex(0);
     }
-    //SELECT link_id, owner, owner_comment, created_time, title, summary, url, image_urls FROM link WHERE owner=me()
+    FB.api("me/posts",{
+        limit:25,
+        since: (new Date(latest_post.created_time).getTime())/1000
+    },loadingPrevFBPostLoop);
+
+} else {
+    var loadedPosts = [];
+
+    $(".Loading").html("페이스북 정보를 로딩중입니다. 이 로딩은 초기 한번만 실행됩니다");
+    var loadingNextFBPostLoop = function(response){
+        loadedPosts = _.union(loadedPosts,response.data);
+        _.each(response.data, createPostDiv);
+        if(response.data.length > 0 && response.paging.next){
+            $.getJSON(response.paging.next,loadingNextFBPostLoop);
+            $(".Loading").append("<p>" + response.data.length + "</p>");
+        }
+        else {
+            console.log("loading finished");
+            alert("글 로딩이 끝났습니다");
+            localStorage.loaded_posts = JSON.stringify(loadedPosts);
+            localStorage.latest_post  = JSON.stringify(_.first(loadedPosts));
+            $(".Loading").fadeOut();
+        }
+    }
+    FB.api("me/posts",{limit:200},loadingNextFBPostLoop);
+}
+
+
+function FacebookContentsManager()
+{
+    this.posts = [];
+}
+FacebookContentsManager.prototype = {
+
 };
+
+
+*/
---
+++ public/js/FBDiary/template_loader.js
@@ -0,0 +1,7 @@
+//using underscore.js
+//using jQuery.js
+
+function TemplateLoader(container){
+    this.$container = $(container);
+
+}
