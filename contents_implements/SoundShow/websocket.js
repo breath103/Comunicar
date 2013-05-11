172026e5116fc5667c17e84d7ea75ccbab9573a3
breath103 <breath103@gmail.com>
2013-05-11 19:18:13 +0900

    WIP - soundshow control Pannel

--- contents_implements/SoundShow/websocket.js
+++ contents_implements/SoundShow/websocket.js
@@ -2,9 +2,15 @@ var fs = require("fs");
 module.exports = function(content,io){
 	var viewerRoom = "viewRoom";
 	
+	var lastScreenColor = null;
 	io.of('/' + content.name).on('connection', function (socket) {
+		if (lastScreenColor) {
+			socket.emit("screen_color",lastScreenColor);
+		}
+	
 		socket.on("screen_color",function(data){
 			console.log("new Color : ", data);
+			lastScreenColor = data;
 			socket.broadcast.emit("screen_color",data);
 		});
 		socket.on('message', function (data) {
